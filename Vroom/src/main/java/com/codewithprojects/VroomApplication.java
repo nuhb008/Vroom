675eaba614e182392c8d3af84aff513b8757fa11
Nuh Islam <nuhislam@iut-dhaka.edu>
2024-11-16 02:01:10 +0600

    1st

---
+++ Vroom/src/main/java/com/codewithprojects/VroomApplication.java
@@ -0,0 +1,13 @@
+package com.codewithprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VroomApplication.class, args);
+	}
+
+}
